fdb5d724484ea94e4703a702e0a5461cced3eba9
AbdullhGaber <abdullh760@tahoo.com>
2026-05-19 20:23:51 +0300

    Initial Commit

---
+++ sports_appTests/sports_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  sports_appTests.swift
+//  sports_appTests
+//
+//  Created by Abdullh Gaber on 19/05/2026.
+//
+
+import Testing
+@testable import sports_app
+
+struct sports_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
